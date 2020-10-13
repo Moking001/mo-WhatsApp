c51fa18cc1048cfef7e2c10564ec64b0727bae33
Moking001 <68329910+Moking001@users.noreply.github.com>
2020-10-13 13:14:55 +0430

    Add files via upload

---
+++ moking.sh
@@ -0,0 +1,26 @@
+
+
+
+#Mod by        : Moking
+#Tool Unfaedah : h4ker;"m"
+
+clear
+
+                                          
+
+ echo $mer"               WhatsappHack by moking | t.me/h4kermoking              "
+
+
+ echo ""
+ echo -n $cy" Enter Whatsapp Number for hack: "
+ read t
+ echo $pur"<•=============h4ket============•>"
+ echo -n $cy" Click on verification on ok Whatsapp and then tipe ok: "
+ read t
+ echo $pur"<•===========Moking============•>"
+ echo $me "Verification Code loading "
+ sleep 10
+ 
+ #!/bin/bash
+
+ echo Code:$((RANDOM + 456907))
